32141d8f8c561910afe1c9b3070307dcb677daff
@EtikaKL <etikalaeli@gmail.com>
2020-11-19 21:55:56 +0700

    modelbarangmasuk

---
+++ backend/models/BarangMasuk.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "barang_masuk".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $supplier_id
+ * @property string $tgl_masuk
+ *
+ * @property Supplier $supplier
+ * @property User $user
+ * @property DetailBarangMasuk[] $detailBarangMasuks
+ */
+class BarangMasuk extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'barang_masuk';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'supplier_id', 'tgl_masuk'], 'required'],
+            [['user_id', 'supplier_id'], 'integer'],
+            [['tgl_masuk'], 'safe'],
+            [['supplier_id'], 'exist', 'skipOnError' => true, 'targetClass' => Supplier::className(), 'targetAttribute' => ['supplier_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'supplier_id' => 'Supplier ID',
+            'tgl_masuk' => 'Tgl Masuk',
+        ];
+    }
+
+    /**
+     * Gets query for [[Supplier]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSupplier()
+    {
+        return $this->hasOne(Supplier::className(), ['id' => 'supplier_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * Gets query for [[DetailBarangMasuks]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDetailBarangMasuks()
+    {
+        return $this->hasMany(DetailBarangMasuk::className(), ['barang_masuk_id' => 'id']);
+    }
+}
